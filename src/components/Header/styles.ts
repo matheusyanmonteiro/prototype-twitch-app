127f1b21fe09883d2d3475d3892bf38b37846655
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2020-09-21 18:38:36 -0300

    created components and coming soon page

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+`;
+
+export const Avatar = styled.View``;
+
+export const OnlineStatus = styled.View``;
+
+export const RightSide = styled.View``;
+
+export const Button = styled.View``;
---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Header from '../../components/Header';
+import { Wrapper, Container, Main } from './styles';
+
+const Following: React.FC = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <Header />
+        <Main />
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Following;
